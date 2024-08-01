4e75880ecc78f1cec7e2cb71a1e43839b405e6ee
senioritosly <mariocardona0501@gmail.com>
2024-08-01 10:46:52 -0600

    Emisor

--- Emisor/CRC-32.py
+++ Emisor/CRC-32.py
@@ -1,16 +1,23 @@
+from functools import reduce
 import crcmod.predefined
+import util as util
 
 def CRC32(trama):
     polinomio = "crc-32"
-    crc_func = crcmod.predefined.Crc(polinomio)
+
+        crc_func = crcmod.predefined.Crc(polinomio)
+
     trama_bytes = bytes(int(trama[i:i+8], 2) for i in range(0, len(trama), 8))
+
     crc_final = crc_func.new(trama_bytes).digest()
-    crc_bin = bin(int.from_bytes(crc_final, byteorder='big'))[2:].zfill(32)
+    crc_bin = ' '.join(format(byte, '08b') for byte in crc_final)
+
     return crc_bin
 
 def simulacion(trama):
-    crc_temporal = trama[-32:]  
+    crc_temporal = trama[-32:] 
     trama_sin_crc = trama[:-32]  
+
     crc_calculado_recibido = CRC32(trama_sin_crc)
 
     if crc_calculado_recibido == crc_temporal:
@@ -19,12 +26,68 @@ def simulacion(trama):
         print("Se detectaron errores en la trama.")
     
 def main():
-    trama = input("Ingrese la trama en binario: ")
-    crc_calculado = CRC32(trama)
-    trama_con_crc = trama + crc_calculado
-
-    print("Trama con CRC-32:", trama_con_crc)
+    #Pedir mensaje
+    trama = input("Ingrese el texto a enviar: ")
+    print("Mensaje a enviar: "+trama)
+    print("")
+    #Codificar mensaje
+    nueva_trama = util.char_to_extended_ascii_bits(trama)
+    print("La trama en ascii: "+nueva_trama)
+    print("")
     
+    crc_calculado = CRC32(nueva_trama)
+    trama_con_crc = nueva_trama + crc_calculado
+    print("Trama con CRC-32:", trama_con_crc)
+    print("")
+
+    trama_final = util.add_ruido(trama_con_crc)
+    print("La trama con ruido: "+ trama_final)
+    print("")
+
+def layer_implementation(msg_input) -> str:
+
+    trama = reduce(
+        (lambda acc, val: acc + val),
+        [util.char_to_extended_ascii_bits(char) for char in msg_input]
+    )
+
+    encoded_trama = CRC32(trama)
+    
+    trama_ruido, cambios = util.add_ruido(encoded_trama)
+    
+    return trama_ruido, cambios 
 
 if __name__ == "__main__":
-    main()
+    import socket
+    from pruebas import pruebas
+    s = socket.socket()
+        
+    HOST = "127.0.0.1" 
+    PORT = 65432         
+    
+    s.connect((HOST, PORT))
+
+    num_exitos = 0
+    num_fracasos = 0
+    
+    print('---- Iniciando pruebas ----')
+    for i in range(1000):
+        for msg_input in pruebas:
+            trama, cambios = layer_implementation(msg_input)
+
+            s.send(trama.encode())
+            response = s.recv(1024).decode('utf-8')
+            if response == "1" and cambios != 0 :
+                num_exitos += 1
+            else:
+                num_fracasos += 1
+
+        if 100 * (i + 1) % 10000 == 0:
+            print(f'> {100 * (i + 1)} pruebas realizadas')
+
+    print('\nExitos:', num_exitos)
+    print('fracasos:', num_fracasos)
+    porcentaje = (num_exitos / 100000) * 100
+    porcentaje = round(porcentaje, 2)
+    print(f'precision: {porcentaje}%')
+    s.close()
--- Emisor/Hamming.py
+++ Emisor/Hamming.py
@@ -1,33 +1,110 @@
-def get_parity_bit(trama, index):
-    subtrama = [trama[i] for i in range(len(trama)) if (i + 1) & (1 << index)]
-    return sum(subtrama) % 2
+from functools import reduce
+from util import *
 
-def hamming_encode(trama):
-    n = len(trama)
-    m = 0
-    while 2 ** m < n + m + 1:
-        m += 1
+P0_LIST = [2, 4, 6]
+P1_LIST = [2, 5, 6]
+P2_LIST = [4, 5, 6]
 
-    mensaje_codificado = [0] * (n + m)
-
-    j = 0
-    for i in range(1, n + m + 1):
-        if i == 2 ** j:
-            mensaje_codificado[i - 1] = get_parity_bit(trama + mensaje_codificado, j)
-            j += 1
+def get_trama_parity(trama:list) -> list[int|None]:
+    new_trama = []
+    
+    for i in range(7):
+        if i in [0, 1, 3]:
+            new_trama.append(None)
         else:
-            mensaje_codificado[i - 1] = trama.pop(0)
+            new_trama.append(trama.pop(-1))
+    
+    return new_trama
 
-    return "".join(str(bit) for bit in mensaje_codificado)
+def get_parity(p_list: list[int], trama: list[int|None]) -> int:
+    bit_map = [trama[i] for i in p_list]
+    return 0 if bit_map.count(1) % 2 == 0 else 1
+
+def hamming_encode(trama: list[int]) -> str:
+    trama = get_trama_parity(trama)
+
+    trama[0] = get_parity(P0_LIST, trama)
+    trama[1] = get_parity(P1_LIST, trama)
+    trama[3] = get_parity(P2_LIST, trama)
+    
+    return reduce(
+        (lambda acc, val: str(val) + str(acc)),
+        trama
+    )
+
+def process_hamming(trama:str) -> str:
+    trama = [int(bit) for bit in trama]
+    sub_tramas = []
+
+    while len(trama) > 0:
+        new_sub_trama = [
+            0 if len(trama) == 0 else trama.pop(0)
+                for _ in range(4)
+        ]
+
+        sub_tramas.append(new_sub_trama)
+
+    encoded_tramas = [hamming_encode(trama) for trama in sub_tramas]
+
+    mensaje_devided:str = reduce(
+        (lambda acc, val: acc + ' ' + val),
+        encoded_tramas
+    )
+
+    mensaje:str = reduce(
+        (lambda acc, val: acc + val),
+        encoded_tramas
+    )
+    return mensaje, mensaje_devided
 
 
-def main():
-    trama_input = input("Ingrese una trama en binario: ")
-    trama = [int(bit) for bit in trama_input]
+def layer_implementation(msg_input) -> str:
 
-    mensaje_codificado = hamming_encode(trama)
+    trama = reduce(
+        (lambda acc, val: acc + val),
+        [char_to_extended_ascii_bits(char) for char in msg_input]
+    )
 
-    print("Trama codificada con Hamming:", mensaje_codificado)
+    encoded_trama, encoded_print = process_hamming(trama)
+    
+    trama_ruido, cambios = add_ruido(encoded_trama)
+    
+    return trama_ruido
+
 
 if __name__ == "__main__":
-    main()
+    import socket
+    from pruebas import pruebas
+    s = socket.socket()
+
+        
+    HOST = "127.0.0.1"  
+    PORT = 65432              
+    
+    s.connect((HOST, PORT))
+
+    num_exitos = 0
+    num_fracasos = 0
+
+    print('---- Iniciando pruebas ----')
+    for i in range(1000):
+        for msg_input in pruebas:
+            # Aplicar arquitectura de capas
+            trama = layer_implementation(msg_input)
+
+            s.send(trama.encode())
+            response = s.recv(1024).decode('utf-8')
+            if response == msg_input:
+                num_exitos += 1
+            else:
+                num_fracasos += 1
+
+        if 100 * (i + 1) % 10000 == 0:
+            print(f'> {100 * (i + 1)} pruebas realizadas')
+
+    print('\nExitos:', num_exitos)
+    print('fracasos:', num_fracasos)
+    porcentaje = (num_exitos / 100000) * 100
+    porcentaje = round(porcentaje, 2)
+    print(f'precision: {porcentaje}%')
+    s.close()
